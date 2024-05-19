8d08296e6fab3f8f22eab7ea857856695f892976
Brian <brian.q.tram@gmail.com>
2024-05-19 05:52:26 -0400

    Revert "BillBoard Added"
    This reverts commit 443a670ff15837c4bbf94ceb21fff6e7d5186add.

--- hush/Assets/BillBoard.cs
+++ hush/Assets/BillBoard.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class BillBoard : MonoBehaviour
 {
-    private Transform cam;
+    // Start is called before the first frame update
     void Start()
     {
-        cam = Camera.main.transform;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(transform.position = cam.forward);
+        
     }
 }
